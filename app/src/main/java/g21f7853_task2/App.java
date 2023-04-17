cd9d8f2f7e4fbb4fb2c08fe469ad01a17821aebd
Jaydin <jfischat13@gmail.com>
2023-04-17 20:01:14 +0200

    Modified to use a SAX XML parser instead

--- app/src/main/java/g21f7853_task2/App.java
+++ app/src/main/java/g21f7853_task2/App.java
@@ -1,74 +1,83 @@
 package g21f7853_task2;
 
-import java.io.File;
-import java.io.IOException;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 public class App {
+
     public static void main(String[] args) {
         try {
-            List<String> validFields = Arrays.asList("name", "postalZip", "region", "country", "address", "list");
-            List<String> fieldsToOutput = readUserSelectedFields(validFields);
-            File inputFile = new File("src/main/resources/data.xml");
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(inputFile);
-            doc.getDocumentElement().normalize();
-            NodeList nList = doc.getElementsByTagName("record");
-            JSONArray recordsArray = new JSONArray();
-            for (int temp = 0; temp < nList.getLength(); temp++) {
-                Node nNode = nList.item(temp);
-                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                    Element eElement = (Element) nNode;
-                    JSONObject recordObject = new JSONObject();
-                    for (String field : fieldsToOutput) {
-                        Node fieldNode = eElement.getElementsByTagName(field).item(0);
-                        if (fieldNode != null) {
-                            String fieldValue = fieldNode.getTextContent();
-                            recordObject.put(field, fieldValue);
-                        }
-                    }
-                    recordsArray.put(recordObject);
-                }
-            }
-            System.out.println(recordsArray.toString());
-        } catch (ParserConfigurationException | SAXException | IOException | JSONException e) {
-            System.err.println("Error: " + e.getMessage());
+            List<String> fieldsToOutput = readUserSelectedFields();
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            RecordHandler recordHandler = new RecordHandler(fieldsToOutput);
+            saxParser.parse("src/main/resources/data.xml", recordHandler);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static List<String> readUserSelectedFields(List<String> validFields) {
-        List<String> fieldsToOutput = new ArrayList<String>();
-        try {
-            System.out.print("Enter comma-separated fields to output: ");
-            String input = System.console().readLine();
-            String[] inputFields = input.split(",");
-            for (String inputField : inputFields) {
-                String field = inputField.trim();
-                if (validFields.contains(field)) {
-                    fieldsToOutput.add(field);
-                } else {
-                    System.err.println("Error: Invalid field: " + field);
-                }
+    private static List<String> readUserSelectedFields() {
+        List<String> validFields = Arrays.asList("name", "postalZip", "region", "country", "address", "list");
+        List<String> fieldsToOutput = new ArrayList<>();
+        System.out.println("Enter comma-separated fields to output:");
+        String input = System.console().readLine();
+        String[] fields = input.split(",");
+        for (String field : fields) {
+            String trimmedField = field.trim();
+            if (validFields.contains(trimmedField)) {
+                fieldsToOutput.add(trimmedField);
+            } else {
+                System.out.println("Invalid field: " + trimmedField);
             }
-        } catch (NullPointerException e) {
-            System.err.println("Error: Console not available. Exiting.");
-            System.exit(1);
         }
         return fieldsToOutput;
     }
+
+    private static class RecordHandler extends DefaultHandler {
+        private List<String> fieldsToOutput;
+        private JSONObject currentRecord;
+        private String currentTag;
+
+        public RecordHandler(List<String> fieldsToOutput) {
+            this.fieldsToOutput = fieldsToOutput;
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (qName.equalsIgnoreCase("record")) {
+                currentRecord = new JSONObject();
+            }
+            currentTag = qName;
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            String value = new String(ch, start, length).trim();
+            if (!value.isEmpty()) {
+                if (fieldsToOutput.contains(currentTag)) {
+                    currentRecord.put(currentTag, value);
+                }
+            }
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (qName.equalsIgnoreCase("record")) {
+                JSONArray recordArray = new JSONArray();
+                recordArray.put(currentRecord);
+                System.out.println(recordArray.toString());
+            }
+        }
+    }
+
 }
